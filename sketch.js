a4e1d30b1f0007d9c65cacb86283a819b56fac3d
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-12-01 15:21:31 -0300

    01/12

--- sketch.js
+++ sketch.js
@@ -1,38 +1,53 @@
 //cria as variáveis
 var trex, trexCorrendo;
 var solo,imagem;
+var inv;
 function preload(){
 //Carrega as imagens para a animação do T-rex
 trexCorrendo = loadAnimation("trex1.png","trex3.png","trex4.png");
 imagem=loadImage("ground2.png")
 }
+
 function setup(){
 //Cria a tela do nosso jogo
 createCanvas(1200,400);
+solo=createSprite(200,380,400,20)
+solo.addImage(imagem);
+solo.x=solo.width/2;      
 //cria o sprite do T-rex e adiciona a animação
 trex = createSprite(50,160,20,50);
 trex.addAnimation("correndo",trexCorrendo);
 //cria as bordas
 borda = createEdgeSprites();
-solo=createSprite(200,380,400,20)
-solo.addImage(imagem);
-solo.x=solo.width/2;
+
+inv=createSprite(200,390,400,10);
+inv.visible=false
+
+//var numero = Math.round(random(1,50));
+//console.log(numero);
 }
 function draw(){
 //pinta o fundo da tela do jogo de branco
 background("black");
+//console.log(trex.y);     
 solo.velocityX=-2;
 if(solo.x<0){
 solo.x=solo.width/2;
 }
 //Faz o trex pular quando aperta a tecla espaço
-if(keyDown("space")){
-trex.velocityY = -10;
+if(keyDown("space")&&trex.y>=300){
+trex.velocityY = -13;
 }
 //Sistema degravidade
 trex.velocityY = trex.velocityY + 1;
 //Impede que o T-rex caia da tela
-trex.collide(solo);
+trex.collide(inv);
+//chamar uma função que desenha as nuvens
+gerarNuvens();
 //Desenha todos os sprites
 drawSprites();
+}
+
+function gerarNuvens(){
+    
 }
